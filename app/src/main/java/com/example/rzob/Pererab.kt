9f88949912ba71c71bfdd758614c77b4197794ef
gefro <gefro47@gmail.com>
2020-11-26 16:32:31 +0300

    Добавлена кнопка удалить activity_pererab.xml, так же добавлен функционал этой кнопки в Pererab.kt Добавлено чтение из БД, если данные есть на выбранный день, они отображаются в textview и появляется кнопка удалить, а после удаления она исчизает и textview заполняется "0.0" значением. Если данных нет в БД на выбранный день, то кнопка удалить не доступна, а textview заполняется "0.0" значением.

--- app/src/main/java/com/example/rzob/Pererab.kt
+++ app/src/main/java/com/example/rzob/Pererab.kt
@@ -3,13 +3,17 @@ package com.example.rzob
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_pererab.*
 import java.math.BigDecimal
 
@@ -38,7 +42,7 @@ class Pererab : AppCompatActivity() {
         var day = intent.getStringExtra("Day")
         var month = intent.getStringExtra("Month")
         var year = intent.getStringExtra("Year")
-
+        val uid = FirebaseAuth.getInstance().uid
 
 //        textView3.text = uid
         textView.text = "$day.$month.$year"
@@ -53,9 +57,68 @@ class Pererab : AppCompatActivity() {
             save_data()
         }
 
+        delete_btn.setOnClickListener {
+            delete_data()
+        }
 
+        delete_btn.visibility = View.INVISIBLE
+
+
+        var getdata_hours = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                var data_hours15 = p0.child("Часы С Коэф 1,5").value
+                if (data_hours15 == null) {
+                    hours_edittext.setText("0.0")
+                    Toast.makeText(applicationContext, "Часов переработки\n нету", Toast.LENGTH_SHORT).show()
+                } else {
+                    delete_btn.visibility = View.VISIBLE
+                    var data_hours2 = p0.child("Часы С Коэф 2").value
+                    if (data_hours2 == null){
+                        val hours2 = "0.0"
+                        val hours15 = data_hours15.toString()
+                        val d_hours15 = hours15.toDouble()
+                        val d_hours2 = hours2.toDouble()
+                        var data_hours = d_hours15 + d_hours2
+                        hours_edittext.setText("$data_hours")
+                    }else{
+                        var hours2 = data_hours2.toString()
+                        val hours15 = data_hours15.toString()
+                        val d_hours15 = hours15.toDouble()
+                        val d_hours2 = hours2.toDouble()
+                        var data_hours = d_hours15 + d_hours2
+                        hours_edittext.setText("$data_hours")
+                    }
+                }
+            }
+        }
+//        FirebaseDatabase.getInstance().getReference("users/$uid/$year/$month/$day")
+//                .addValueEventListener(getdata_hours)
+        FirebaseDatabase.getInstance().getReference("users/$uid/$year/$month/$day")
+                .addListenerForSingleValueEvent(getdata_hours)
 
     }
+
+    fun delete_data(){
+        val intent = intent
+        var day = intent.getStringExtra("Day")
+        var month = intent.getStringExtra("Month")
+        var year = intent.getStringExtra("Year")
+        val uid = FirebaseAuth.getInstance().uid
+        FirebaseDatabase.getInstance().getReference("users/$uid")
+                .child("$year").child("$month").child("$day").removeValue()
+                .addOnSuccessListener {
+                    hours_edittext.setText("0.0")
+                    Toast.makeText(applicationContext, "Успешно удалено", Toast.LENGTH_SHORT).show()
+                    delete_btn.visibility = View.INVISIBLE
+                }
+                .addOnFailureListener {
+                    Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
+                }
+    }
+
     fun save_data(){
         val intent = intent
         var day = intent.getStringExtra("Day")
@@ -66,48 +129,45 @@ class Pererab : AppCompatActivity() {
         if (hours.toString() != ""){
             val hours1 = hours.toString()
             var Hours1 = hours1.toDouble()
-            if (Hours1 != null) {
-                if (Hours1 <= 2.0){
-                    val hours15 = hours1
-                    FirebaseDatabase.getInstance().getReference("users/$uid")
-                        .child("$year").child("$month").child("$day").child("Часы С Коэф 1,5").setValue(hours15)
-                            .addOnSuccessListener {
-                                Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
-                                Toast.makeText(applicationContext, "Часы С Коэф 1,5:     $hours15", Toast.LENGTH_SHORT).show()
-                            }
-                            .addOnFailureListener {
-                                Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
-                            }
+            if (Hours1 <= 2.0){
+                val hours15 = hours1
+                FirebaseDatabase.getInstance().getReference("users/$uid")
+                    .child("$year").child("$month").child("$day").child("Часы С Коэф 1,5").setValue(hours15)
+                        .addOnSuccessListener {
+//                            Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Сохранено\nЧасы С Коэф 1,5:     $hours15", Toast.LENGTH_SHORT).show()
+                            delete_btn.visibility = View.VISIBLE
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
+                        }
 //                        textView3.text = hours15
-                }
             }
-
-            if (Hours1 != null) {
-                if (Hours1 > 2.0){
-                    val hours15 = "2.0"
-                    val x : BigDecimal = BigDecimal(2)
-                    val hours2 = Hours1.toBigDecimal()
-                    val result = hours2.subtract(x)
+            if (Hours1 > 2.0){
+                val hours15 = "2.0"
+                val x : BigDecimal = BigDecimal(2)
+                val hours2 = Hours1.toBigDecimal()
+                val result = hours2.subtract(x)
 //                        textView3.text = "$result"
-                    FirebaseDatabase.getInstance().getReference("users/$uid")
-                        .child("$year").child("$month").child("$day").child("Часы С Коэф 1,5").setValue(hours15)
-                            .addOnSuccessListener {
-                                Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
-                                Toast.makeText(applicationContext, "Часы С Коэф 1,5:    $hours15", Toast.LENGTH_SHORT).show()
-                            }
-                            .addOnFailureListener {
-                                Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
-                            }
-                    FirebaseDatabase.getInstance().getReference("users/$uid")
-                        .child("$year").child("$month").child("$day").child("Часы С Коэф 2").setValue(result.toString())
-                            .addOnSuccessListener {
-                                Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
-                                Toast.makeText(applicationContext, "Часы С Коэф 2:     $result", Toast.LENGTH_SHORT).show()
-                            }
-                            .addOnFailureListener {
-                                Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
-                            }
-                }
+                FirebaseDatabase.getInstance().getReference("users/$uid")
+                    .child("$year").child("$month").child("$day").child("Часы С Коэф 1,5").setValue(hours15)
+                        .addOnSuccessListener {
+//                            Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Сохранено\nЧасы С Коэф 1,5:    $hours15", Toast.LENGTH_SHORT).show()
+                            delete_btn.visibility = View.VISIBLE
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
+                        }
+                FirebaseDatabase.getInstance().getReference("users/$uid")
+                    .child("$year").child("$month").child("$day").child("Часы С Коэф 2").setValue(result.toString())
+                        .addOnSuccessListener {
+//                            Toast.makeText(applicationContext, "Сохранено", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(applicationContext, "Сохранено\nЧасы С Коэф 2:     $result", Toast.LENGTH_SHORT).show()
+                        }
+                        .addOnFailureListener {
+                            Toast.makeText(applicationContext, "Ошибка, попробуй позже!", Toast.LENGTH_SHORT).show()
+                        }
             }
         }
     }
